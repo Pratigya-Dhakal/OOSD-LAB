003eca660f6756c276ad64d9bdbad41d4c632c2f
Pratigya-Dhakal <pratigyadhakal4444@gmail.com>
2022-06-18 17:02:38 +0545

    OOSD LAB 1 on singeleton

---
+++ SingletonPatternDemo.java
@@ -0,0 +1,10 @@
+public class SingletonPatternDemo {
+   public static void main(String[] args) {
+
+
+      SingleObject object = SingleObject.getInstance();
+
+      //show the message
+      object.showMessage();
+   }
+}
